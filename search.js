5a77515aa866435981520c9853016d1f09a56d92
webmalc <m@webmalc.pw>
2019-02-21 15:17:10 +0300

    add binary search

--- search.js
+++ search.js
@@ -13,5 +13,27 @@ module.exports = {
             }
         }
         return -1;
+    },
+
+    /**
+     * Binary search O(logn)
+     * @param {array} arr - sorted array for search
+     * @param {*} val - value to search
+     * @return {int} index of element (-1 if not found)
+     */
+    binarySearch: function (arr, val) {
+        var start = 0;
+        var end = arr.length - 1;
+        var middle = Math.floor(start / 2);
+        while(arr[middle] !== val && start <= end) {
+            if(val < arr[middle]) {
+                end = middle - 1; 
+            }
+            else {
+                start = middle + 1;
+            }
+            middle = Math.floor((start + end) / 2);
+        }
+        return arr[middle] === val ? middle : -1;
     }
 };
--- test/search.js
+++ test/search.js
@@ -1,12 +1,31 @@
 const assert = require('assert');
 let search = require('../search');
+let utils = require('./utils');
 
 describe('search', function() {
 
+    let longTest = function(func) {
+        const max = 100000;
+        const arr = Array.apply(null, {length: max}).map(Number.call, Number);
+        assert.equal(func(arr, 50000), 50000, 'long array');
+    }; 
+
     it('test linear search', function() {
         let func = search.linearSearch;
-        assert(func([10, 23, 3, 4, 5], 23), 1);
-        assert(func([10, 23], 33), -1);
-        assert(func([100], 33), 0);
+        assert.equal(func([10, 23, 3, 4, 5], 23), 1);
+        assert.equal(func([10, 23], 33), -1);
+        assert.equal(func([100], 33), -1);
+        longTest(func);
+        
+    });
+
+    it('test binary search', function() {
+        let func = search.binarySearch;
+        assert.equal(func([1, 2, 3, 4, 5], 3), 2);
+        assert.equal(func([1, 2, 3, 4, 5], 0), -1);
+        assert.equal(func([1, 2, 3, 4, 5, 6], 5), 4);
+        assert.equal(func([1, 2, 3, 4, 5, 6], 1), 0);
+        assert.equal(func([], -1), -1);
+        longTest(func);
     });
 });
